7ff8d2590e206a65e9b28377dba003d70ba48e6f
iamgirya <84345515+iamgirya@users.noreply.github.com>
2025-07-10 14:42:37 +0300

    urls

--- lib/presentation/widgets/contacts_widget.dart
+++ lib/presentation/widgets/contacts_widget.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class ContactInfo {
+  final String icon;
+  final String text;
+  final String url;
+
+  const ContactInfo({
+    required this.icon,
+    required this.text,
+    required this.url,
+  });
+}
 
 class ContactsWidget extends StatelessWidget {
   const ContactsWidget({super.key});
+
+  static const List<ContactInfo> _contacts = [
+    ContactInfo(
+      icon: 'assets/telegram.svg',
+      text: 'Vikaefrb',
+      url: 'https://t.me/Vikaefrb',
+    ),
+    ContactInfo(
+      icon: 'assets/mail.svg',
+      text: 'viktoriababachenko@yandex.ru',
+      url: 'mailto:viktoriababachenko@yandex.ru',
+    ),
+    ContactInfo(
+      icon: 'assets/be.svg',
+      text: 'www.behance.net/victoriababachenko',
+      url: 'https://www.behance.net/victoriababachenko',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,39 +45,36 @@ class ContactsWidget extends StatelessWidget {
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          _buildContactItem(
-            context,
-            icon: 'assets/telegram.svg',
-            text: 'Vikaefrb',
-          ),
-          _buildContactItem(
-            context,
-            icon: 'assets/mail.svg',
-            text: 'viktoriababachenko@yandex.ru',
-          ),
-          _buildContactItem(
-            context,
-            icon: 'assets/be.svg',
-            text: 'www.behance.net/victoriababachenko',
-          ),
-        ],
+        children:
+            _contacts
+                .map((contact) => _buildContactItem(context, contact))
+                .toList(),
       ),
     );
   }
 
-  Widget _buildContactItem(
-    BuildContext context, {
-    required String icon,
-    required String text,
-  }) {
+  Future<void> _launchUrl(String url) async {
+    final uri = Uri.parse(url);
+    if (!await launchUrl(uri)) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  Widget _buildContactItem(BuildContext context, ContactInfo contact) {
     final textTheme = Theme.of(context).textTheme;
-    return Row(
-      children: [
-        SvgPicture.asset(icon, width: 24, height: 24),
-        const SizedBox(width: 10),
-        Text(text, style: textTheme.bodyMedium),
-      ],
+    return InkWell(
+      onTap: () => _launchUrl(contact.url),
+      borderRadius: BorderRadius.circular(10),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            SvgPicture.asset(contact.icon, width: 24, height: 24),
+            const SizedBox(width: 10),
+            Text(contact.text, style: textTheme.bodyMedium),
+          ],
+        ),
+      ),
     );
   }
 }
